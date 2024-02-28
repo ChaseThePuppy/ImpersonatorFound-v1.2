88c71d2878f6c8fe3a8aac16db61b68765f172fd
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-28 17:01:37 +0500

    Create keyword.lua

---
+++ keyword.lua
@@ -0,0 +1,3 @@
+    if key == a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z then print("KEY PRESSED")
+elseif aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ then print(local KEYPRESSED, "KEYPRESSED")
+end
